8b862c3d04b2a1413ec6fa245260d7e67abf03f4
MinJaeSim <mj9757@gmail.com>
2018-08-01 16:36:37 +0900

    로그인 구현

--- app/src/main/java/yellow7918/ajou/ac/aunager/MainActivity.kt
+++ app/src/main/java/yellow7918/ajou/ac/aunager/MainActivity.kt
@@ -1,12 +1,24 @@
 package yellow7918.ajou.ac.aunager
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.TimeUnit
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        TimeUnit.SECONDS.sleep(10)
+
+        Snackbar.make(constraint_layout, "SIGN OUT", Snackbar.LENGTH_SHORT).show()
+        FirebaseAuth.getInstance().signOut()
+
+        startActivity(Intent(this@MainActivity, SignInActivity::class.java))
     }
 }
---
+++ app/src/main/java/yellow7918/ajou/ac/aunager/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package yellow7918.ajou.ac.aunager
+
+import android.os.Bundle
+
+
+class SignUpActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
