b590a19c7b728249ac95966d039e5429e03b3061
MinJaeSim <mj9757@gmail.com>
2018-08-01 13:14:10 +0900

    project init

---
+++ app/src/main/java/yellow7918/ajou/ac/aunager/MainActivity.kt
@@ -0,0 +1,12 @@
+package yellow7918.ajou.ac.aunager
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
